8ef30c26e4167f671ca12c9e77932e707b5c802c
Cécile MORANGE <contact@ataxya.net>
2023-07-07 09:08:24 +0200

    remove ToolsIsoName

--- builder/xenserver/common/common_config.go
+++ builder/xenserver/common/common_config.go
@@ -76,10 +76,6 @@ func (c *CommonConfig) Prepare(ctx *interpolate.Context, pc *common.PackerConfig
 		c.RawBootWait = "5s"
 	}
 
-	if c.ToolsIsoName == "" {
-		c.ToolsIsoName = ""
-	}
-
 	if c.HTTPPortMin == 0 {
 		c.HTTPPortMin = 8000
 	}
